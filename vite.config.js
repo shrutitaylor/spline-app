b0a9a4a4073bca8dc22d6f99e1f3c535586be83c
shrutitaylor <shruti128770@gmail.com>
2025-06-17 16:21:05 +1000

    gh pages - version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
+
 export default defineConfig({
   plugins: [react(),ghPages()],
   // base: "/portfolio-template-js/",
