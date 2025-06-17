ca9847cb48320a67d92797d106e9d39e734ef032
shrutitaylor <shruti128770@gmail.com>
2025-06-17 16:19:30 +1000

    final changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import ghPages from 'vite-plugin-gh-pages';
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),ghPages()],
   // base: "/portfolio-template-js/",
+   base: '/spline-app/',
   css: {
     postcss: './postcss.config.js', // Ensure PostCSS is linked
   },
